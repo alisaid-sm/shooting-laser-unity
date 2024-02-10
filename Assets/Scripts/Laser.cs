e3aedbfbd1feedfae6d021554885aeaa287ad524
alisaid-sm <alisaid7170@gmail.com>
2024-02-10 10:45:12 +0700

    feat: add analog to control laser rotation
    - fix bug laser code

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class Laser : MonoBehaviour
     public float laserLength = 10f;
     public GameObject hitLimiter;
     public GameObject DamageArea;
+    public FixedJoystick fixedJoystick;
     private LineRenderer lr;
 
     // Start is called before the first frame update
@@ -19,9 +21,22 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = fixedJoystick.Horizontal;
+        float vertical = fixedJoystick.Vertical;
+
+        Vector3 rotationDirection = new(horizontal, 0, vertical);
+
+        rotationDirection = Vector3.Normalize(rotationDirection);
+
+        if (rotationDirection != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(rotationDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 50*Time.deltaTime);
+        }
+
         lr.SetPosition(0, transform.position);
 
-        //LaserV1();
         LaserV2();
     }
 
@@ -29,7 +44,9 @@ public class Laser : MonoBehaviour
     {
         RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, laserLength);
 
-        for (int i = hits.Length - 1; i > -1; i--)
+        Array.Sort(hits, (hit1, hit2) => hit1.distance.CompareTo(hit2.distance));
+
+        for (int i = 0; i < hits.Length; i++)
         {
             if (hits[i].collider.name != "DamageArea")
             {
